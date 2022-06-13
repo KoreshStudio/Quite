0c0602e6dc078125907d2779be725aca03eea219
KoreshStudio <106616526+KoreshStudio@users.noreply.github.com>
2022-06-14 01:44:23 +0300

    Create footer.php

---
+++ blocks/footer.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+</head>
+
+<body>
+
+<div id="a">
+</div>
+
+<div id="header">
+<center><img src="img/logo.png" width="400px" height="400px"></center>
+<center>Koresh Studio 2018-2022</center>
+<center><h3>Использованные технологии: Php, Html, Css</h3></center>
+<center><h3>Использованный шрифт: Nunito</h3></center>
+<center><a href="https://vk.com/quitesite">Мы во ВКонтакте</a></center>
+</div>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,600;1,900&display=swap');
+
+#a {
+	border-bottom: 1px solid silver;
+}
+  
+body {
+  font-family: 'Nunito', sans-serif;
+}
+
+</style>
+</body>
+
+</html>
